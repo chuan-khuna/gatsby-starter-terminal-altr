d39cb1d1158fa03a38b0cd753ebf04c37cca03f6
ALTR <chuan.khuna@gmail.com>
2021-12-23 21:08:10 +0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    name: "gatsby-starter-terminal",
+    description: "A Gatsby starter for @pauliescanlon/gatsby-theme-terminal",
+    keywords: ["tech", "blog", "boop"],
+    siteUrl: "https://gatsby-starter-terminal.netlify.com",
+    siteImage: "terminal-open-graph-image.jpg",
+    profileImage: ``,
+    lang: `en`,
+    config: {
+      sidebarWidth: 280
+    }
+  },
+  plugins: [
+    {
+      resolve: "@chuan-khuna/gatsby-theme-terminal-altr",
+      options: {
+        source: [{name: "posts", dir: "posts"}]
+      }
+    }
+  ]
+};
